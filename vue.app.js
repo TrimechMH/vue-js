5e5f9678f579aea9e3179612a53129c6c0734844
TrimechMH <trimech.med.habib@gmail.com>
2017-10-28 13:05:52 +0200

    24 : refactor & watching value.

--- vue.app.js
+++ vue.app.js
@@ -5,14 +5,14 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World!',
-        link : 'http://google.com',
-        finishedLink : '<a href="http://google.com"> Google</a>'
+        link: 'http://google.com',
+        finishedLink: '<a href="http://google.com"> Google</a>'
     },
     methods: {
-        changeTitle: function(event) {
+        changeTitle: function (event) {
             this.title = event.target.value;
         },
-        sayHello : function () {
+        sayHello: function () {
             this.title = 'Hello!';
             return this.title;
         }
@@ -22,33 +22,41 @@ new Vue({
 new Vue({
     el: '#app1',
     data: {
-       counter : 0,
-       counterDI : 0,
-       secondCounter : 0,
-        x :0,
-        y : 0
+        counter: 0,
+        counterDI: 0,
+        secondCounter: 0,
+        x: 0,
+        y: 0
     },
     computed: {
-      output: function () {
-          console.log('computed');
-          return this.counterDI > 5 ? 'Greater than5' : 'Smaller than 5'
-      }
+        output: function () {
+            console.log('computed');
+            return this.counterDI > 5 ? 'Greater than5' : 'Smaller than 5'
+        }
+    },
+    watch: {
+        counterDI: function (value) {
+            var vm = this;
+            setTimeout(function(){
+                vm.counterDI = 0;
+            },2000)
+        }
     },
     methods: {
-        increase: function(step) {
+        increase: function (step) {
             this.counter += step;
         },
-        updateCoordinate : function (event) {
+        updateCoordinate: function (event) {
             this.x = event.clientX;
             this.y = event.clientY;
         },
         dummy: function (event) {
             event.stopPropagation();
         },
-        alertMe : function () {
+        alertMe: function () {
             alert('alert! you press enter || space ||F2  ...');
         },
-        result: function(){
+        result: function () {
             console.log('result');
             return this.counterDI > 5 ? 'Greater than5' : 'Smaller than 5'
         }
@@ -57,10 +65,9 @@ new Vue({
 new Vue({
     el: '#app2',
     data: {
-        name : 'Max'
+        name: 'Max'
     },
-    methods: {
-    }
+    methods: {}
 });
 
 //define new keyCodes
