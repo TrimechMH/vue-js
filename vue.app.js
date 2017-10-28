dc995afebd9707320a2f05be7a6d57158f9e102a
TrimechMH <trimech.med.habib@gmail.com>
2017-10-28 11:55:30 +0200

    19 stop propagation.

--- vue.app.js
+++ vue.app.js
@@ -33,6 +33,9 @@ new Vue({
         updateCoordinate : function (event) {
             this.x = event.clientX;
             this.y = event.clientY;
+        },
+        dummy: function (event) {
+            event.stopPropagation();
         }
     }
 });
