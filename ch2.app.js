89d7805ee8d9a053940e44f9dfbd8a6514d2958e
TrimechMH <trimech.med.habib@gmail.com>
2017-12-04 00:02:18 +0100

    034 Conditional Rendering with v-if

--- ch2.app.js
+++ ch2.app.js
@@ -2,24 +2,6 @@
  * Created by med-habib.trimech on 6/20/2017.
  */
 new Vue({
-    el: '#app',
-    data: {
-        title: 'Hello World!',
-        link: 'http://google.com',
-        finishedLink: '<a href="http://google.com"> Google</a>'
-    },
-    methods: {
-        changeTitle: function (event) {
-            this.title = event.target.value;
-        },
-        sayHello: function () {
-            this.title = 'Hello!';
-            return this.title;
-        }
-    }
-});
-
-new Vue({
     el: '#app1',
     data: {
         counter: 0,
---
+++ ch3.app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by med-habib.trimech on 6/20/2017.
+ */
+new Vue({
+    el: '#app',
+    data: {
+        show: true
+    },
+
+});
